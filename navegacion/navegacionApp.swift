a42569bbdf0fc5d0678d182dc5a24d052307e3d4
yhrslzr <yahir.salazarortiz0113@gmail.com>
2025-09-29 17:53:17 -0600

    Initial Commit

---
+++ navegacion/navegacionApp.swift
@@ -0,0 +1,17 @@
+//
+//  navegacionApp.swift
+//  navegacion
+//
+//  Created by Yahir Salazar on 29/09/25.
+//
+
+import SwiftUI
+
+@main
+struct navegacionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
