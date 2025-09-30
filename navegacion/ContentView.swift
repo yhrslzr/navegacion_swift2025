d985335a456c63c5f7e5eb87e2abd05fd5f65911
yhrslzr <yahir.salazarortiz0113@gmail.com>
2025-09-29 18:43:11 -0600

    navegación funcional sin parametros

--- navegacion/ContentView.swift
+++ navegacion/ContentView.swift
@@ -9,13 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack{
+            Text("Bienvenido a la Nintendo Store")
+            
+            Spacer()
+            NavigationStack{
+                NavigationLink(destination: ProductView()){
+                    ItemRowView()
+                }
+                Spacer()
+                Text("Desarrollado por: Yahir Slzr 🪼")
+                
+            }
         }
-        .padding()
     }
 }
 
